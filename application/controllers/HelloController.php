490b26ed0e5555867ca2fb435e60241dbdafd695
NewDeveloper111 <96533885+NewDeveloper111@users.noreply.github.com>
2024-02-24 17:21:08 +0300

    Добавлены примеры консольных команд в документацию (#28)
    Документация и примеры консольных команд

--- application/controllers/HelloController.php
+++ application/controllers/HelloController.php
@@ -11,5 +11,18 @@ class HelloController
     {
         echo 'HELLO !', PHP_EOL;
     }
-}
 
+    public function calcAction()
+    {
+        echo 1 + 2, PHP_EOL;
+    }
+
+    public function echoAction()
+    {
+        global $argv;
+        echo 'Вы ввели команду: "' . $argv[1] . '"', PHP_EOL;
+        if (count($argv) == 3) {
+            echo 'Дополнительный аргумент: "' . $argv[2] . '"', PHP_EOL;
+        }
+    }
+}
